3a4c862fc80cac1451ad2a9fd2a210d50d136c0e
Daniel Muñoz <daniel.munozr@adevinta.com>
2021-06-08 17:57:40 +0200

    mock by user

---
+++ mock/tony/defaultCompanies.js
@@ -0,0 +1,200 @@
+export default {
+  companies: [
+    {
+      id: '1',
+      name: 'Blissey',
+      requirements: [
+        {
+          investorRequirementId: '1',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '2',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '3',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '4',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '5',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '6',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '7',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '8',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '9',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '10',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '11',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '12',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '13',
+          value: 'YES'
+        }
+      ],
+      web: 'blissey.com'
+    },
+    {
+      id: '2',
+      name: 'Yveltal',
+      requirements: [
+        {
+          investorRequirementId: '1',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '2',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '8',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '10',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '13',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '15',
+          value: 'NO'
+        }
+      ],
+      web: 'yveltal.com'
+    },
+    {
+      id: '3',
+      name: 'Registeel',
+      requirements: [
+        {
+          investorRequirementId: '1',
+          value: 'NO'
+        },
+        {
+          investorRequirementId: '2',
+          value: 'NO'
+        },
+        {
+          investorRequirementId: '6',
+          value: 'NO'
+        },
+        {
+          investorRequirementId: '7',
+          value: 'NO'
+        },
+        {
+          investorRequirementId: '13',
+          value: 'NO'
+        },
+        {
+          investorRequirementId: '6',
+          value: 'NO'
+        }
+      ],
+      web: 'registeel.com'
+    },
+    {
+      id: '4',
+      name: 'Lugia',
+      requirements: [
+        {
+          investorRequirementId: '1',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '2',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '3',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '4',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '5',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '6',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '7',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '8',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '9',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '10',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '11',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '12',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '13',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '14',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '15',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '16',
+          value: 'YES'
+        },
+        {
+          investorRequirementId: '17',
+          value: 'YES'
+        }
+      ],
+      web: 'lugia.com'
+    }
+  ]
+}
---
+++ mock/tony/defaultRequirements.js
@@ -0,0 +1,86 @@
+export default {
+  requirements: [
+    {
+      id: '1',
+      criteria: 'Stage is Seed or Series A',
+      priority: 'MUST'
+    },
+    {
+      id: '2',
+      criteria: 'Industry is FinTech or InsurTech',
+      priority: 'MUST'
+    },
+    {id: '3', criteria: 'Location is Bay Area', priority: 'SUPER_NICE'},
+    {id: '4', criteria: 'Founding Team is full-time', priority: 'NICE'},
+    {
+      id: '5',
+      criteria: '$0.5M < Size of the round < $2M',
+      priority: 'NICE'
+    },
+    {
+      id: '6',
+      criteria: 'Source of lead is a trusted referrer',
+      priority: 'MUST'
+    },
+    {
+      id: '7',
+      criteria: 'CEO is a serial entrepreneur',
+      priority: 'MUST'
+    },
+    {
+      id: '8',
+      criteria: 'CEO studied at top-tier school',
+      priority: 'NICE'
+    },
+    {
+      id: '9',
+      criteria: 'CTO has led a technical team for 2+ years',
+      priority: 'NICE'
+    },
+    {
+      id: '10',
+      criteria: 'CTO studied at top-tier school',
+      priority: 'SUPER_NICE'
+    },
+    {
+      id: '11',
+      criteria: 'CEO and CTO have known each other for 2+ years',
+      priority: 'SUPER_NICE'
+    },
+    {
+      id: '12',
+      criteria: 'Previous top-tier investors',
+      priority: 'NICE'
+    },
+    {
+      id: '13',
+      criteria: 'Founding team has experience in the industry',
+      priority: 'MUST'
+    },
+    {
+      id: '14',
+      criteria: 'Current investors are coinvestors in our portfolio companies',
+      priority: 'SUPER_NICE'
+    },
+    {
+      id: '15',
+      criteria: 'The entire team is based in the same location',
+      priority: 'NICE'
+    },
+    {
+      id: '16',
+      criteria: 'Relevant press coverage',
+      priority: 'NICE'
+    },
+    {
+      id: '17',
+      criteria: 'IP and patents',
+      priority: 'NICE'
+    },
+    {
+      id: '18',
+      criteria: 'Hires from top-tier companies in the industry',
+      priority: 'NICE'
+    }
+  ]
+}
